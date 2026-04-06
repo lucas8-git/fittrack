535e8e91086f06d53355c017b33c6c07953dc724
lucas8-git <lucashs888@gmail.com>
2026-04-06 21:39:19 +0200

    Create route.ts

---
+++ app/api/register/route.ts
@@ -0,0 +1,48 @@
+import { prisma } from "@/lib/db";
+import { hash } from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { email, password } = body;
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "Missing email or password" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name: email.split("@")[0],
+      },
+    });
+
+    return NextResponse.json(
+      { message: "User created successfully", userId: user.id },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Registration failed" },
+      { status: 500 }
+    );
+  }
+}
