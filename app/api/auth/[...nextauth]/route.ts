b5a6657946ec0b16959da77d58d72f1c88b29c41
lucas8-git <lucashs888@gmail.com>
2026-04-07 20:20:48 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,3 @@
-import { auth as authConfig } from "@/auth";
-import NextAuth from "next-auth";
+import { handlers } from "@/auth";
 
-const handler = NextAuth(authConfig);
-
-export { handler as GET, handler as POST };
+export const { GET, POST } = handlers;
